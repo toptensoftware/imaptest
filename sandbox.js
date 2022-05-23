f543e19e9352d8849b93a0309f1c07a690662ba2
Brad Robinson <contact@toptensoftware.com>
2022-05-23 16:25:58 +1000

    Working on folder view

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,45 @@
-const Database = require('./lib/Database');
 const Utils = require('./lib/Utils');
+const assert = require('assert');
 
-(async function(){
+let o = [10, 20, 30];
+let n = [1, 2, -10, 15, 16, 25, 26, 33, 35, -40];
 
-    await Database.open({
-        db_server: "mongodb://localhost:44017/?directConnection=true", 
-        db_name: "sandbox"
-    });
+console.log(JSON.stringify(o));
+console.log(JSON.stringify(n));
 
-    let coll = Database.db.collection("test");
+function compare(a,b) {
+    if (a == b)
+        return 0;
+    if (a == -b)
+        return null;
+    return a - b;
+}
 
-    /*
-    await coll.insertOne({
-        flags: 0
-    });
-    */
-
-    await coll.updateMany(
-        { },
-        { $bit: { flags: { or: 2 } }  }
-    )
+// Generate edits
+let edits = Utils.generate_diff(o, n, compare);
+edits.forEach(x => console.log(JSON.stringify(x)));
 
 
-    Database.close();
-})();
+// Apply edits
+for (let e of edits)
+{
+    switch (e.op)
+    {
+        case 'i':
+            o.splice(e.oi + e.adjust, 0, n[e.ni]);
+            break;
+
+        case 'd':
+            o.splice(e.oi + e.adjust, 1);
+            break;
+
+        case 'u':
+            o[e.oi + e.adjust] = n[e.ni];
+            break;
+    }
+}
+
+console.log(JSON.stringify(o));
+
+assert.deepEqual(o, n);
+console.log("Success!");
