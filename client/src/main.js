f893c515ebad04fdc0e41b00cb2673c232f8ce69
Brad Robinson <contact@toptensoftware.com>
2022-05-30 16:50:23 +1000

    starting on login

--- client/src/main.js
+++ client/src/main.js
@@ -24,6 +24,33 @@ app.mount('#app')
 
 
 /*
+text editor: https://quilljs.com
+
+html sanitizing: 
+    https://www.npmjs.com/package/sanitize-html 
+    https://www.w3schools.com/tags/att_iframe_sandbox.asp
+    https://blog.chromium.org/2010/05/security-in-depth-html5s-sandbox.html (detect support for iframe sandbox)
+    https://stackoverflow.com/questions/27852405/html-attributes-that-can-contain-javascript
+    https://www.npmjs.com/package/css (css parser to find url() url(javascript:) styles)
+
+content encoding: https://www.npmjs.com/package/http-encoding
+
+quoted printable: https://www.npmjs.com/package/quoted-printable
+
+invert content for dark mode: https://giancarlobuomprisco.com/css/automatic-dark-mode-any-website
+
+getting css value in javascript: https://zellwk.com/blog/css-values-in-js/
+    (perhaps use this to determine if selected theme is dark/light and whether to try to modify email presentation)
+
+preventing csrf https://portswigger.net/web-security/csrf/samesite-cookies
+
+rest apis:
+    * https://stackoverflow.blog/2020/03/02/best-practices-for-rest-api-design/
+    * https://stackoverflow.blog/2021/10/06/best-practices-for-authentication-and-authorization-for-rest-apis/
+
+*/
+
+/*
 
 TODO:
 
